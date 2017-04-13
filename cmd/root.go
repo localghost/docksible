ff03de4fb44ea29d0f918d57b530246b073e7697
Zbigniew Kostrzewa <kostrzewa@9livesdata.com>
2017-04-13 07:50:44 +0200

    Add CLI flags.

--- cmd/root.go
+++ cmd/root.go
@@ -2,22 +2,17 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"os"
-	"log"
 	"fmt"
+	"github.com/localghost/docksible/product"
 )
 
-type topLevelFlags struct {
+type rootFlags struct {
 	ansibleDir string
 	playbookPath string
-}
-
-func getCwd() string {
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return dir
+	inventoryGroups []string
+	extraArgs []string
+	baseImage string
+	builderImage string
 }
 
 func printUsageError(cmd *cobra.Command, message string) {
@@ -25,17 +20,26 @@ func printUsageError(cmd *cobra.Command, message string) {
 }
 
 func CreateRootCommand() *cobra.Command {
-	flags := topLevelFlags{}
+	flags := rootFlags{}
 	cmd := &cobra.Command{
-		Use: "docksible [flags] playbook",
+		Use: "docksible [flags] image playbook",
 		Run: func (cmd *cobra.Command, args []string) {
-			if len(args) != 1 {
-				printUsageError(cmd, "Please provide path to playbook to execute.")
+			if len(args) != 2 {
+				printUsageError(cmd, "Please provide path to playbook to execute and image to provision.")
 			}
+			run(args[0], args[1], &flags)
 		},
 	}
-	cmd.Flags().StringVarP(&flags.ansibleDir, "ansible-dir", "a", getCwd(), "Path to ansible directory.")
+	cmd.Flags().StringVarP(&flags.ansibleDir, "ansible-dir", "a", "", "Path to ansible directory.")
+	cmd.Flags().StringSliceVarP(&flags.inventoryGroups, "inventory-groups", "g", []string{}, "Ansible groups the provisioned container should belong to.")
+	cmd.Flags().StringSliceVarP(&flags.extraArgs, "extra-args", "x", []string{}, "Extra arguments passed to ansible.")
+	cmd.Flags().StringVarP(&flags.builderImage, "builder-image", "b", "docksible/builder", "Docker image for the builder container. It needs to have ansible and ssh client built in.")
 
 	return cmd
 }
 
+func run(image, playbook string, flags *rootFlags) {
+	builder := builder.New()
+	builder.Bootstrap()
+	builder.ProvisionContainer(product.New().Run())
+}
--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"log"
 	"github.com/localghost/docksible/cmd"
-	"github.com/localghost/docksible/builder"
-	"github.com/localghost/docksible/product"
+	//"github.com/localghost/docksible/builder"
+	//"github.com/localghost/docksible/product"
+	"os"
+	"fmt"
 )
 
 func main() {
 	if err := cmd.CreateRootCommand().Execute(); err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	builder := builder.New()
-	builder.Bootstrap()
-	builder.ProvisionContainer(product.New().Run())
+	//builder := builder.New()
+	//builder.Bootstrap()
+	//builder.ProvisionContainer(product.New().Run())
 }
